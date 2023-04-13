2e8cc24bdcd0b3f020d5339fd0ac0ae6286155bc
MissVeronica <veronica@tranza.se>
2023-04-13 16:39:42 +0200

    Update um-oxygen-email-templates.php

--- um-oxygen-email-templates.php
+++ um-oxygen-email-templates.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Ultimate Member - Oxygen Builder Email Templates
  * Description:     Extension to Ultimate Member for integration of UM email templates with Oxygen Builder.
- * Version:         1.0.0
+ * Version:         2.0.0
  * Requires PHP:    7.4
  * Author:          Miss Veronica
  * License:         GPL v2 or later
@@ -17,84 +17,111 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 if ( ! class_exists( 'UM' ) ) return;
 
-define( 'THEME_OXYGEN', '/theme-oxygen/ultimate-member/email/' );
+class UM_Oxygen_Builder_Email_Templates {
 
 
-remove_filter( 'um_change_settings_before_save', array( UM()->admin_settings(), 'save_email_templates' ) );
+    function __construct( ) {
+        
+        $email_templates_oxygen_folder = trim( sanitize_text_field( UM()->options()->get( 'email_templates_oxygen_folder' )));
+        if ( empty( $email_templates_oxygen_folder )) {
+            $email_templates_oxygen_folder = '/theme-oxygen/ultimate-member/email/';
+        }
+        define( 'THEME_OXYGEN', $email_templates_oxygen_folder );
 
-add_filter( 'um_change_settings_before_save', 'save_email_templates_oxygen', 10, 1 );
+        remove_filter( 'um_change_settings_before_save', array( UM()->admin_settings(), 'save_email_templates' ) );
 
-add_filter( 'um_get_template',          'um_get_template_oxygen', 10, 4 );
-add_filter( 'um_locate_template',       'um_locate_template_oxygen', 10, 3 );
-add_filter( 'um_locate_email_template', 'um_locate_email_template_oxygen', 10, 2 );
+        add_filter( 'um_change_settings_before_save', array( $this, 'save_email_templates_oxygen' ), 10, 1 );
+        add_filter( 'um_settings_structure',          array( $this, 'um_settings_structure_templates_oxygen' ), 10, 1 );
+        add_filter( 'um_get_template',                array( $this, 'um_get_template_oxygen' ), 10, 4 );
+        add_filter( 'um_locate_template',             array( $this, 'um_locate_template_oxygen' ), 10, 3 );
+        add_filter( 'um_locate_email_template',       array( $this, 'um_locate_email_template_oxygen' ), 10, 2 );
+    }
 
-function um_get_template_oxygen( $located, $template_name, $path, $t_args ) {
+    public function um_get_template_oxygen( $located, $template_name, $path, $t_args ) {
 
-    $template_oxygen = WP_CONTENT_DIR . THEME_OXYGEN . $template_name . ".php";
-    if( file_exists( $template_oxygen )) return $template_oxygen;
-    return $located;
- }
+        $template_oxygen = WP_CONTENT_DIR . THEME_OXYGEN . $template_name . ".php";
+        if( file_exists( $template_oxygen )) return $template_oxygen;
+        return $located;
+    }
 
-function um_locate_template_oxygen( $template, $template_name, $path ) {
+    public function um_locate_template_oxygen( $template, $template_name, $path ) {
 
-    $template_oxygen = WP_CONTENT_DIR . THEME_OXYGEN . $template_name . ".php";
-    if( file_exists( $template_oxygen )) return $template_oxygen;
-    return $template;
-}
+        $template_oxygen = WP_CONTENT_DIR . THEME_OXYGEN . $template_name . ".php";
+        if( file_exists( $template_oxygen )) return $template_oxygen;
+        return $template;
+    }
 
-function um_locate_email_template_oxygen( $template, $template_name ) {
+    public function um_locate_email_template_oxygen( $template, $template_name ) {
 
-    $template_oxygen = WP_CONTENT_DIR . THEME_OXYGEN . $template_name . ".php";
-    if( file_exists( $template_oxygen )) return $template_oxygen;
-    return $template;
-}
+        $template_oxygen = WP_CONTENT_DIR . THEME_OXYGEN . $template_name . ".php";
+        if( file_exists( $template_oxygen )) return $template_oxygen;
+        return $template;
+    }
 
-function save_email_templates_oxygen( $settings ) {
+    public function save_email_templates_oxygen( $settings ) {
 
-    if ( empty( $settings['um_email_template'] ) ) {
+        if ( empty( $settings['um_email_template'] ) ) {
+            return $settings;
+        }
+
+        $template = $settings['um_email_template'];
+        $content = wp_kses_post( stripslashes( $settings[ $template ] ) );
+
+        $theme_template_path = trailingslashit( WP_CONTENT_DIR . THEME_OXYGEN ) . $template . '.php';
+        if ( ! file_exists( $theme_template_path ) ) {       
+
+            $plugin_template_path = trailingslashit( um_path . 'templates/email/' ) . $template . '.php';
+            $temp_path = str_replace( WP_CONTENT_DIR .'/', '', $theme_template_path );
+            $temp_path = str_replace( '/', DIRECTORY_SEPARATOR, $temp_path );
+
+            $folders = explode( DIRECTORY_SEPARATOR, $temp_path );
+            $folders = array_splice( $folders, 0, count( $folders ) - 1 );
+            $cur_folder = '';
+            $theme_dir = trailingslashit( WP_CONTENT_DIR );
+
+            foreach ( $folders as $folder ) {
+                $prev_dir = $cur_folder;
+                $cur_folder .= $folder . DIRECTORY_SEPARATOR;
+                if ( ! is_dir( $theme_dir . $cur_folder ) && wp_is_writable( $theme_dir . $prev_dir ) ) {
+                    mkdir( $theme_dir . $cur_folder, 0777 );
+                }
+            }
+
+            if ( file_exists( $plugin_template_path ) && is_writable( WP_CONTENT_DIR . THEME_OXYGEN )) {
+                copy( $plugin_template_path, $theme_template_path );
+            }
+        }
+
+        if ( is_writable( $theme_template_path )) {
+            $fp = fopen( $theme_template_path, "w" );
+            if( $fp !== false ) {
+                $result = fputs( $fp, $content );
+                fclose( $fp );
+
+                if ( $result !== false ) {
+                    unset( $settings['um_email_template'] );
+                    unset( $settings[ $template ] );
+                }
+            }
+        }
         return $settings;
     }
 
-    $template = $settings['um_email_template'];
-    $content = wp_kses_post( stripslashes( $settings[ $template ] ) );
+    public function um_settings_structure_templates_oxygen( $settings_structure ) {
 
-    $theme_template_path = trailingslashit( WP_CONTENT_DIR . THEME_OXYGEN ) . $template . '.php';
-    if ( ! file_exists( $theme_template_path ) ) {       
+        $settings_structure['email']['fields'][] =
+ 
+                array(
+                    'id'          => 'email_templates_oxygen_folder',
+                    'type'        => 'text',
+                    'label'       => __( 'Email_Templates - Customzed Email Folder Path', 'ultimate-member' ),
+                    'tooltip'     => __( 'Enter the folder path where you will save your customized email templates. Example: /theme-oxygen/ultimate-member/email/ ', 'ultimate-member' ),
+                    'size'        => 'medium',
+                    'default'     => '/theme-oxygen/ultimate-member/email/',
+                );
 
-        $plugin_template_path = trailingslashit( um_path . 'templates/email/' ) . $template . '.php';
-        $temp_path = str_replace( WP_CONTENT_DIR .'/', '', $theme_template_path );
-        $temp_path = str_replace( '/', DIRECTORY_SEPARATOR, $temp_path );
-
-        $folders = explode( DIRECTORY_SEPARATOR, $temp_path );
-        $folders = array_splice( $folders, 0, count( $folders ) - 1 );
-        $cur_folder = '';
-        $theme_dir = trailingslashit( WP_CONTENT_DIR );
-
-        foreach ( $folders as $folder ) {
-            $prev_dir = $cur_folder;
-            $cur_folder .= $folder . DIRECTORY_SEPARATOR;
-            if ( ! is_dir( $theme_dir . $cur_folder ) && wp_is_writable( $theme_dir . $prev_dir ) ) {
-                mkdir( $theme_dir . $cur_folder, 0777 );
-            }
-        }
-
-        if ( file_exists( $plugin_template_path ) && is_writable( WP_CONTENT_DIR . THEME_OXYGEN )) {
-            copy( $plugin_template_path, $theme_template_path );
-        }
+        return $settings_structure;
     }
-
-    if ( is_writable( $theme_template_path )) {
-        $fp = fopen( $theme_template_path, "w" );
-        if( $fp !== false ) {
-            $result = fputs( $fp, $content );
-            fclose( $fp );
-
-            if ( $result !== false ) {
-                unset( $settings['um_email_template'] );
-                unset( $settings[ $template ] );
-            }
-        }
-    }
-    return $settings;
 }
 
+new UM_Oxygen_Builder_Email_Templates();
